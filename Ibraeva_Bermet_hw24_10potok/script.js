289c936c74d912171151d817aed731b1ccedea98
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-09-25 18:02:48 +0600

    homeworks

---
+++ Ibraeva_Bermet_hw24_10potok/script.js
@@ -0,0 +1,32 @@
+"use strict";
+//использоваие строго режима для обработки данных
+
+// 23 urok
+
+// let age = 29;
+// let year = 1993;
+// let name = "Bermet"  
+  
+// console.log(age);
+// console.log(year);
+// console.log(name);
+
+
+//24 urok
+
+let person = {
+    name: "Bermet",
+    family_name: "Ibraeva",
+    age: 29,
+    year_of_born: 1993,
+    location: "Kyrgyzstan"
+
+};
+
+console.log("Name: " + person.name +","+ " Family name: " + person.family_name + "," +
+ " Age: " + person.age  + ","+ " Year of born: " + person.year_of_born +","+ 
+ " Location:" + person.location);
+
+let school = ["maths","physics","chemistry","geography"];
+
+console.log(school[0],school[1],school[2],school[3]);
