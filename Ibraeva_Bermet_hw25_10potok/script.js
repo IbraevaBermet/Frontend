3f9acf9092d53cb723de9e9c1a3d71cd9de21f58
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-09-26 07:34:57 +0600

    js

---
+++ Ibraeva_Bermet_hw25_10potok/script.js
@@ -0,0 +1,60 @@
+"use strict";
+//использоваие строго режима для обработки данных
+
+// 23 urok
+
+// let age = 29;
+// let year = 1993;
+// let name = "Bermet"  
+  
+// console.log(age);
+// console.log(year);
+// console.log(name);
+
+
+//24 urok
+
+
+// let person = {
+//     name: "Bermet",
+//     family_name: "Ibraeva",
+//     age: 29,
+//     year_of_born: 1993,
+//     location: "Kyrgyzstan"
+
+// };
+
+// console.log("Name: " + person.name +","+ " Family name: " + person.family_name + "," +
+//  " Age: " + person.age  + ","+ " Year of born: " + person.year_of_born +","+ 
+//  " Location:" + person.location);
+
+// let school = ["maths","physics","chemistry","geography"];
+
+// console.log(school[0],school[1],school[2],school[3]);
+
+//25 urok
+
+//Задание 1
+
+// let name = prompt("Введите ваше имя: ", );
+// let surname = prompt("Введите ваше фамилие: ", );
+
+// console.log(name,surname);
+
+//Задание 2
+
+// let number1 = Number(prompt("Введите первую цифру: ", ));
+// let number2 = Number(prompt("Введите вторую цифру: ", ));
+// let summa = number1 + number2;
+
+// alert(summa);
+
+
+//Задание 3
+
+let number1 = Number(prompt("Введите первую цифру: ", ));
+let number2 = Number(prompt("Введите вторую цифру: ", ));
+let summa = number1 * number2;
+
+alert(summa);
+
