3fefe44b5f9fdd56d280fd612c90d9ee5ace83e3
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-09-29 05:15:13 +0600

    hw28

--- Ibraeva_Bermet_hw28_10potok/script/add_to_card.js
+++ Ibraeva_Bermet_hw28_10potok/script/add_to_card.js
@@ -1,3 +1,11 @@
 let ttl = document.getElementsByClassName("ttl"); 
 
 console.log(ttl[0].innerText);
+
+let price = document.getElementsByClassName("price"); 
+
+console.log(price[0].innerText);
+
+// let image = document.getElementsByClassName("image").getElementsByTagName("img")[0].src; 
+
+// alert(image);
