81c8f757b1aea1a4769d82434d76c209f4200b1c
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-10-01 20:10:38 +0600

    29 urok

--- Ibraeva_Bermet_hw29_10potok/script/add_to_card.js
+++ Ibraeva_Bermet_hw29_10potok/script/add_to_card.js
@@ -1,11 +1,18 @@
-let ttl = document.getElementsByClassName("ttl"); 
+// let ttl = document.getElementsByClassName("ttl"); 
 
-console.log(ttl[0].innerText);
+// console.log(ttl[0].innerText);
 
-let price = document.getElementsByClassName("price"); 
+// let price = document.getElementsByClassName("price"); 
 
-console.log(price[0].innerText);
+// console.log(price[0].innerText);
 
-// let image = document.getElementsByClassName("image").getElementsByTagName("img")[0].src; 
+// let image = document.getElementsByClassName("image"); 
 
 // alert(image);
+
+
+//29 urok
+
+let ttl = document.querySelectorAll(".body_product > .ttl");
+
+console.log(ttl);
