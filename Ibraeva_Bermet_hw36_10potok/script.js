2733c4756d66a45b58678dc76f85f0652e429b9f
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-10-24 04:39:29 +0600

    urok 36

---
+++ Ibraeva_Bermet_hw36_10potok/script.js
@@ -0,0 +1,59 @@
+"use strict";
+//использоваие строго режима для обработки данных
+
+//ООП - понятие
+//Применение
+
+//Как понять что есть строка
+
+let str = "текст";
+let strObj = new String(str);
+
+//console.log(typeof(str));
+//console.log(strObj);
+
+let array = [1,2,3];
+
+//console.log(array);
+
+let car = {
+    kuzov:"Железо",
+    kolesa:"4 колеса",
+    modal: function (){
+        alert("Hello")
+    }
+};
+
+
+
+let mb = {
+    marka:"Мерс",
+    god:"2021"
+};
+
+//mb.__proto__ = car;
+Object.setPrototypeOf(mb,car);
+
+//console.log(car);
+//console.log(mb.modal);
+
+//mb.modal();
+
+
+//Задание №1
+
+let salary = {
+    almaz: 1200,
+    adilet: 1300,
+    nursultan: 3000
+};
+
+let bonus = {
+    almaz: 400,
+    adilet: 300,
+    nursultan: 200
+};
+
+salary.__proto__= bonus;
+
+console.log(salary.almaz);
