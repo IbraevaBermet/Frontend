81a86eb6201ed6254d41cd6f9abc49a71b0975ea
Bermet Ibraeva <ibraevabermet093@mail.ru>
2022-11-02 01:04:46 +0600

    urok 40

---
+++ Ibraeva_Bermet_hw40_10potok/script.js
@@ -0,0 +1,98 @@
+"use strict";
+//использоваие строго режима для обработки данных
+
+//ООП - понятие
+//Применение
+
+//Как понять что есть строка
+
+// let str = "текст";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(strObj);
+
+// let array = [1,2,3];
+
+// console.log(array);
+
+// let car = {
+//     kuzov:"Железо",
+//     kolesa:"4 колеса",
+//     modal: function (){
+//         alert("Hello")
+//     }
+// };
+
+
+
+// let mb = {
+//     marka:"Мерс",
+//     god:"2021"
+// };
+
+//mb.__proto__ = car;
+//Object.setPrototypeOf(mb,car);
+
+//console.log(car);
+//console.log(mb.modal);
+
+//mb.modal();
+
+
+//Задание №1
+
+// let salary = {
+//     almaz: 1200,
+//     adilet: 1300,
+//     nursultan: 3000
+// };
+
+// let bonus = {
+//     almaz: 400,
+//     adilet: 300,
+//     nursultan: 200
+// };
+
+// salary.__proto__= bonus;
+
+// console.log(salary.almaz);
+
+
+// 38 urok
+
+// Динамическая типизация
+
+//let number =+"1";
+//console.log(number + 5);
+//console.log(parseInt(number) + 5);
+//console.log(typeof('1'));
+ 
+//let res_input = +prompt("Введите номер страницы","");
+
+//console.log(typeof(res_input));
+
+//console.log(res_input + 10);
+//if (typeof(res_))
+//console.log(res_input);
+
+//https://miukid.com/cat/
+
+//console.log(`https://miukid.com/cat/${res_input}`);
+
+
+
+// задание №1
+
+//let input = prompt("Введите любое число", );
+
+//function takeInput (){
+    l//et input = prompt("Введите любое число",);
+    //console.log(typeof(input));
+
+
+//}
+
+
+//takeInput();
+
